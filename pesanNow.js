7e2797d3d0d2b6da94d8d29ae3a961cde1b7d44e
mrizqputra <rizkysapp@gmail.com>
2022-09-17 03:41:46 +0700

    17 sept 90% fix

---
+++ pesanNow.js
@@ -0,0 +1,7 @@
+function pesanNow() {
+    alert('Pemesanan Gagal! Website under development.');
+  }
+
+  function feedBack() {
+    alert('Pengiriman Feedback Gagal! Website under development.');
+  }
